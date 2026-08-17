98dc233d31c9249a157011b4b238221443f66d20
Ashfak Ahmath <ashfakahmath57@gmail.com>
2026-08-18 01:31:42 +0530

    feat: add BlogSpace favicon for browser tab

--- includes/header.php
+++ includes/header.php
@@ -9,6 +9,7 @@ if (session_status() === PHP_SESSION_NONE) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BlogSpace</title>
+    <link rel="icon" type="image/png" href="favicon.png">
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
